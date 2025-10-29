2161db54c747cb53722cf49439cd8daea2eb8ad7
Constanza Paez <con.paez@duocuc.cl>
2025-10-29 18:13:19 -0300

    errores resueltos

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ import ProductosDestacados from './Components/productos.js';
 import Nosotros from './Components/Nosotros.js';
 import CalculadoraHuerto from './Components/CalculadoraHuerto.js';
 import Contacto from './Components/Contacto.js';
-<<<<<<< HEAD
 import Servicios from './Components/Servicios.js';
 import BannerPrincipal from './Components/Banner.js';
-=======
->>>>>>> 93aafc57a54478f6c54bcc13baa1d02689aab520
+
 
 function App() {
   return (
@@ -23,10 +21,7 @@ function App() {
         <CalculadoraHuerto/>
         <Servicios/>
         <Nosotros/>
-<<<<<<< HEAD
-=======
         <CalculadoraHuerto/>
->>>>>>> 93aafc57a54478f6c54bcc13baa1d02689aab520
         <Contacto/>
       </div>
 
